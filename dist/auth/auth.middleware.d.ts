8ac160568f301b2e9d5ea18cd508aeabf4508332
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2025-03-04 00:19:08 +0500

    Revert "with dist"
    This reverts commit bbc67b591b8a32c0dbb8bc8783d869c6f0b27be2.

--- dist/auth/auth.middleware.d.ts
+++
@@ -1,8 +0,0 @@
-import { NestMiddleware } from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
-import { UserService } from '../user/user.service';
-export declare class AuthMiddleware implements NestMiddleware {
-    private userService;
-    constructor(userService: UserService);
-    use(req: Request, res: Response, next: NextFunction): Promise<Response<any, Record<string, any>> | undefined>;
-}
--- dist/auth/firebase-auth.middleware.d.ts
+++
@@ -1,7 +0,0 @@
-import { NestMiddleware } from '@nestjs/common';
-import { UserService } from '../user/user.service';
-export declare class FirebaseAuthMiddleware implements NestMiddleware {
-    private readonly userService;
-    constructor(userService: UserService);
-    use(req: any, res: any, next: () => void): Promise<any>;
-}
